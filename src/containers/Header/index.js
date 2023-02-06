18d300a6f4d56fbd72c93d936547b3bb11a5e445
KwonSongji <mariecamillecalone@gmail.com>
2023-02-06 21:47:41 +0100

    img

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -17,9 +17,9 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-       {/*  <div className="gpt1__header-image">
-          <img src={ai} alt="ai" />
-        </div> */}
+         <div className="gpt1__header-image">
+          <img src={ai} alt="ai"/>
+        </div> 
       </div>
     </div>
   )
