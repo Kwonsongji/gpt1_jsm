df9c53ac26a8e44f0ec80b4e44a42e5751401e28
KwonSongji <mariecamillecalone@gmail.com>
2023-02-14 23:28:11 +0100

    feature

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -3,8 +3,14 @@ import './style.css';
 
 const Feature = () => {
   return (
-    <div className='Feature'>
-      Feature
+    <div className="gpt1__features-container__feature">
+      <div className="gpt1__features-container__title">
+        <div /> {/* avoir de l'espace */}
+        <h1>{title }</h1>
+      </div>
+      <div className="gpt1__features-container__title">
+        {text}
+      </div>
     </div>
   )
 }
