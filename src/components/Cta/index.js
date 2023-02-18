34f3ca1097f40929cc2263f94695f7a69b73c5d8
KwonSongji <mariecamillecalone@gmail.com>
2023-02-18 20:56:40 +0100

    cta component done

--- src/components/Cta/index.js
+++ src/components/Cta/index.js
@@ -3,8 +3,14 @@ import './style.css';
 
 const Cta = () => {
   return (
-    <div className='cta'>
-      Cta
+    <div className='gpt1__cta'>
+      <div className="gpt1__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt1__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
     </div>
   )
 }
